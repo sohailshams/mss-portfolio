5b8a2e3d013968189e4336ae3317565c0f4ebda2
sohailshams <sohailshamas@yahoo.com>
2021-09-29 13:37:03 +0200

    Added Home component and added a background image

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import bg from '../assets/images/bg.jpg';
+function Home() {
+  return (
+    <div className="">
+      <img
+        className="mx-auto bg-contain bg-no-repeat bg-center h-screen"
+        src={bg}
+        alt="Background"
+      />
+    </div>
+  );
+}
+
+export default Home;
