2a554fac16fdae9deb3444a2d9e228f2ff65ed9d
sohailshams <sohailshamas@yahoo.com>
2021-09-29 21:57:54 +0200

    Updated nav links and added hover effect and transition effect

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,26 @@
 function Nav() {
   return (
-    <div className="flex items-center justify-center space-x-4 py-4 bg-black text-white font-semibold sticky top-0 z-50">
-      <h4>Home</h4>
-      <h4>About</h4>
-      <h4>Profile</h4>
-      <h4>Resume</h4>
+    <div className="flex items-center justify-center space-x-6 py-4 bg-black text-white font-semibold sticky top-0 z-50">
+      <a href="#">
+        <h4 className="hover:text-gray-400 hover:scale-105 transition transform duration-800 ease-in">
+          Home
+        </h4>
+      </a>
+      <a href="#">
+        <h4 className="hover:text-gray-400 hover:scale-105 transition transform duration-800 ease-in">
+          Profile
+        </h4>
+      </a>
+      <a href="#">
+        <h4 className="hover:text-gray-400 hover:scale-105 transition transform duration-800 ease-in">
+          Contact
+        </h4>
+      </a>
+      <a href="#">
+        <h4 className="hover:text-gray-400 hover:scale-105 transition transform duration-800 ease-in">
+          Resume
+        </h4>
+      </a>
     </div>
   );
 }
