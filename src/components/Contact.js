67a6d0d16f9f7d7838fb8886b0bbc08d27d163e2
sohailshams <sohailshamas@yahoo.com>
2021-10-03 12:46:24 +0200

    Updated tailwindcss configration and added contact form and styled it accordingly

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,45 @@ function Contact() {
   return (
     <div>
       <Nav />
+      <div className="w-10/12 mx-auto">
+        <h2 className="text-center my-4 text-4xl font-semibold">Contact</h2>
+        <h3 className="text-center font-semibold mb-2">
+          Let's bring ideas into reality
+        </h3>
+        <form action="" className="shadow-2xl">
+          <div class="mt-8 max-w-md mx-auto px-4">
+            <div class="grid grid-cols-1 gap-6">
+              <label class="block">
+                <span class="text-gray-700">Full name</span>
+                <input
+                  type="text"
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                  placeholder="Full name"
+                />
+              </label>
+              <label class="block">
+                <span class="text-gray-700">Email address</span>
+                <input
+                  type="email"
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                  placeholder="email@example.com"
+                />
+              </label>
+
+              <label class="block">
+                <span class="text-gray-700">Message</span>
+                <textarea
+                  class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                  rows="3"
+                ></textarea>
+              </label>
+              <div class="block">
+                <button>Send</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
