eecdf2240a683b6766d89e1164b970ada91b6feb
sohailshams <sohailshamas@yahoo.com>
2023-07-16 13:33:34 +0100

    Extracted education to Education component.

---
+++ src/components/Education.jsx
@@ -0,0 +1,20 @@
+function Education() {
+  return (
+    <div className="my-4 text-center">
+      <h3 className="text-lg font-semibold sm:font-extrabold shadow-lg w-10/12 md:w-6/12 mx-auto">
+        Code Institute, Dublin - Ireland
+      </h3>
+      <p className="font-base sm:font-semibold py-2">
+        Diploma in Software Development • December 2020
+      </p>
+      <h3 className="text-lg font-semibold sm:font-extrabold shadow-lg w-10/12 md:w-6/12 mx-auto">
+        Linnæus University, Kalmar - Sweden
+      </h3>
+      <p className="font-base sm:font-semibold py-2">
+        Master in Marketing • December 2010
+      </p>
+    </div>
+  );
+}
+
+export default Education;
