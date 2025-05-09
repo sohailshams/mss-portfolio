82b0ef34d6eb167f2a594838f573c1412fb8e391
sohailshams <sohailshamas@yahoo.com>
2025-05-09 23:17:58 +0100

    Created sanity client.

---
+++ src/sanity/client.js
@@ -0,0 +1,10 @@
+import { createClient } from "@sanity/client";
+
+const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: "2025-05-07",
+  useCdn: true,
+});
+
+export default client;
