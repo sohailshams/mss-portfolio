934c4a37c5376fc89801cdf65908a1dc076b5703
sohailshams <sohailshamas@yahoo.com>
2025-05-12 16:38:20 +0100

    Added react query to fetch soft skills and render accordingly.

--- src/components/Softskills.jsx
+++ src/components/Softskills.jsx
@@ -1,13 +1,25 @@
-import { softSkills } from "../assets/data/resumeData.json";
 import { v1 as uuid } from "uuid";
+import { fetchSoftSkills } from "../utils/utils";
+import { useQuery } from "@tanstack/react-query";
 
 function Softskills() {
+  const {
+    data: softSkills,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["softSkills"],
+    queryFn: async () => fetchSoftSkills(),
+  });
+
+  if (isError || isLoading) return null;
+
   return (
     <div className="w-10/12 mx-auto my-4 grid grid-cols-2 sm:grid-cols-4">
-      {softSkills.softSkillsData.map((skill) => {
+      {softSkills?.map((skill) => {
         return (
           <span key={uuid()} className="border-2 text-center shadow-lg p-1">
-            {skill}
+            {skill.title}
           </span>
         );
       })}
